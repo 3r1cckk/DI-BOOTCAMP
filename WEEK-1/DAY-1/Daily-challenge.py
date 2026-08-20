c417772a73e272d0f1f1499ffa1a1e531b8fde77
3r1cckk <allabouterick0@gmail.com>
2026-08-20 11:19:16 +0300

    Complete daily string challenge

---
+++ WEEK-1/DAY-1/Daily-challenge.py
@@ -0,0 +1,22 @@
+import random
+
+
+user_string = input("Enter a string: ")
+
+if len(user_string) < 10:
+	print("String not long enough.")
+elif len(user_string) > 10:
+	print("String too long.")
+else:
+	print("Perfect string")
+	print(f"First character: {user_string[0]}")
+	print(f"Last character: {user_string[-1]}")
+
+	built_string = ""
+	for character in user_string:
+		built_string += character
+		print(built_string)
+
+	jumbled_characters = list(user_string)
+	random.shuffle(jumbled_characters)
+	print("Jumbled string:", "".join(jumbled_characters))
