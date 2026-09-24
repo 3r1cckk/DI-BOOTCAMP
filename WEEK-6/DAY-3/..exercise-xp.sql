210a85c293971de18b0dd2d86c5b3e3f8ed3846d
3r1cckk <3r1cckk@users.noreply.github.com>
2026-09-24 12:32:21 +0300

    week 6 exercise-xp.sql

--- WEEK-6/DAY-3/..exercise-xp.sql
+++ WEEK-6/DAY-3/..exercise-xp.sql
@@ -118,7 +118,7 @@ JOIN inventory i ON f.film_id = i.film_id
 JOIN rental r ON i.inventory_id = r.inventory_id
 JOIN customer c ON r.customer_id = c.customer_id
 WHERE c.first_name = 'Matthew' AND c.last_name = 'Mahan'
-AND (f.title ILIKE '%boat%' OR f.description ILIKE '%boat%')
+AND f.title ILIKE '%boat%' OR f.description ILIKE '%boat%'
 ORDER BY f.replacement_cost DESC
 LIMIT 1;
 
